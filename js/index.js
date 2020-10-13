28ba5849a8545224712cd40cdeaff4fb84c1fdc9
Emmanuel Flores <sug-040699@hotmail.com>
2020-10-13 18:56:13 -0500

    agregandole funcionalidad al button enviar datos para graficar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 var lineDiv = document.getElementById('line-chart');
+var vgg = document.getElementById('formvgg')
+var vdd = document.getElementById('formvdd')
+var rd = document.getElementById('formrd')
+var dss = document.getElementById('formdss')
+var vp = document.getElementById('formvp')
+var button = document.getElementById('button')
 
-drawGrafiph(16,8e-3,4.75,5.625e-3)
+button.addEventListener('click', dibujoGrafica);
+
+function dibujoGrafica(){
+    var xVoltaje = parseInt(vdd.value*rd.value);
+    console.log(xVoltaje);
+    //var yCorriente = parseInt(vdd/rd)
+    drawGrafiph(16,8e-3,4.75,5.625e-3)
+}
 
 function drawGrafiph(vdd,idmax,vds,id){
     var traceA = {
@@ -29,4 +42,5 @@ function drawGrafiph(vdd,idmax,vds,id){
   };
 Plotly.plot( lineDiv, data, layout);
 }
+
  
