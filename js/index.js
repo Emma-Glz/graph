2d1b92a15373f6788c231357419eb807d7891c01
Emmanuel Flores <sug-040699@hotmail.com>
2020-10-13 20:03:27 -0500

    Operaciones listas y verificadas de xVoltaje, yCorriente y vds(con el detalle de ID) lo unico faltante la operacion de id

--- js/index.js
+++ js/index.js
@@ -9,9 +9,14 @@ var button = document.getElementById('button')
 button.addEventListener('click', dibujoGrafica);
 
 function dibujoGrafica(){
-    var xVoltaje = parseInt(vdd.value*rd.value);
+    var xVoltaje = parseInt(vdd.value);
     console.log(xVoltaje);
-    //var yCorriente = parseInt(vdd/rd)
+    var yCorriente =parseFloat(vdd.value)/parseFloat(rd.value);
+    console.log(yCorriente);
+    var vds = xVoltaje - (parseFloat(rd.value)*parseFloat(5.63e-3));
+    console.log(vds)
+    //var xPunto = parseFloat(vdd.value) - id
+    
     drawGrafiph(16,8e-3,4.75,5.625e-3)
 }
 
